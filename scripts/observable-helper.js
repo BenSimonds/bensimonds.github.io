2ced7ecde330e09f64d914d28f117f3bfe990546
Ben Simonds <bssimonds@gmail.com>
2023-02-24 19:06:42 +0000

    update theme

--- scripts/observable-helper.js
+++ scripts/observable-helper.js
@@ -11,63 +11,47 @@ hexo.extend.helper.register('observable_notebook', function(path, hideAfter=-1){
         var counter = 0;
 
         // Get width to use for observable components
-        const width = targetDiv.getBoundingClientRect().width  - 10;
+        const width = targetDiv.getBoundingClientRect().width  - 25;
 
         const runtime = new Runtime(
             // Override Observable's width property
             Object.assign(new Library, {width})
         );
         const main = runtime.module(notebook, name => {
+            
+            // Create cell div
             const cellOuter = document.createElement('div');
-            cellOuter.classList.add("cellOuter");
-            cellOuter.innerHTML = \`<p class="cellNumber">[\${ name ? name : counter}]</p>\`;
+            cellOuter.classList.add("cell--outer");
+            
+            // When we encounter a viewof node, hide the next sibling with some css
+            if (name && name.startsWith('viewof ')) {
+                cellOuter.classList.add("cell--viewof");
+            }
+
+            // Add Cell Number
+            cellOuter.innerHTML = \`<p class="cell--number">[\${ name ? name : counter}]</p>\`;
             const cellInner = document.createElement('div');
-            cellInner.classList.add("cellInner");
+            cellInner.classList.add("cell--inner");
+
+
             cellOuter.appendChild(cellInner);
+            
+            // Add the content to the correct div.
             if (counter < hideAfter || hideAfter === -1 ) {
                 targetDiv.appendChild(cellOuter)
             } else {
                 appendixDiv.appendChild(cellOuter)
             }
+            
             const inspector =  new Inspector(cellInner);
+            console.log({inspector});
             counter += 1;
             return inspector;
         });
 
         
     </script>
-    <style>
-        .collapseAppendix {
-            display: none;
-        }
-        .cellOuter {
-            position: relative;
-            padding: -1px;
-            border-right: 1px solid;
-            border-radius: 4px;
-            border-color: rgba(200, 200, 200, 0);
-            transition: border-color 1.2s;
-            text-align: right;
-        }
-        .cellOuter:hover {
-            border-color: rgba(200, 200, 200, 1.0);
-        }
-        .cellNumber {
-            position: absolute;
-            top: 0px;
-            left: calc(100% + 5px);
-            opacity: 0.0;
-            font-size: 0.75em;
-            color: rgba(200, 200, 200, 1.0);
-            transition: opacity 1.2s;
-        }
-        .cellOuter:hover .cellNumber {
-            opacity: 1.0;
-        }
-        .cellInner {
-            text-align: left;
-        }
-    </style>
+    
     <div>
     <p>
         This post was first published as an observable notebook. 
